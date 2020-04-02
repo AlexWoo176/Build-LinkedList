28ae6717487ce83b6a10781014bb54c87e46876f
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-04-02 12:07:27 +0700

    Complete

---
+++ src/codegym/exercise/Node.java
@@ -0,0 +1,21 @@
+package codegym.exercise;
+
+public class Node {
+    protected Node next;
+    protected Object element;
+
+    public Node() {
+    }
+
+    public Node(Object element) {
+        this.element = element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+    public Object getElement() {
+        return element;
+    }
+
+}
